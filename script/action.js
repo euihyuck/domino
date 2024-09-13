f2a8dfd812fd5261833e8fe9bb4f7b58b06ea520
euihyuck <euihyk12@naver.com>
2024-09-13 16:31:22 +0900

    2024.09.13

--- script/action.js
+++ script/action.js
@@ -8,6 +8,68 @@ $(document).ready(function () {
 
     $('.login_1').css({ marginTop: height })
 
+
+    $('#section2 li').mouseenter(function () {
+        clearInterval(autoRolling)
+        rollingNumber = $(this).index()
+
+        $(this).addClass('on').siblings().removeClass('on');
+
+        let imgSrc = $(this).find('figure img').attr('src');
+        let h3Text = $(this).find('h3').text();
+        let cateText = $(this).find('.cate').text();
+
+        $('.big_img img').attr('src', imgSrc);
+        $('.big_img img').hide()
+        $('.big_img img').fadeIn()
+
+
+        $('.big_img').removeClass('on')
+
+        setTimeout(function () {
+            $('.big_img').addClass('on')
+            $('.big_img .text strong').text(h3Text)
+            $('.big_img .text .cate').text(cateText)
+        }, 500)
+    });
+
+    $('#section1 li').mouseleave(function () {
+        autoRolling = setInterval(imgRolling, 2000)
+    })
+
+
+
+    // let imgRolling = setInterval(함수, 시간)
+
+    // clearInterval(imgRolling)
+
+    let autoRolling = setInterval(imgRolling, 3000)
+
+    function imgRolling() {
+        rollingNumber++
+        if (rollingNumber >= 4) {
+            rollingNumber = 0
+        }
+
+        $('.small_img li').eq(rollingNumber).addClass('on').siblings().removeClass('on');
+
+        let imgSrc = $('.small_img li').eq(rollingNumber).find('figure img').attr('src');
+        let h3Text = $('.small_img li').eq(rollingNumber).find('h3').text();
+        let cateText = $('.small_img li').eq(rollingNumber).find('.cate').text();
+
+        $('.big_img img').attr('src', imgSrc)
+        $('.big_img img').hide()
+        $('.big_img img').fadeIn()
+
+
+        $('.big_img').removeClass('on')
+
+        setTimeout(function () {
+            $('.big_img').addClass('on')
+            $('.big_img .text strong').text(h3Text)
+            $('.big_img .text .cate').text(cateText)
+        }, 500)
+    }
     //gnb 를 mo_gnb_box에 삽입
     let gnbHtml = $(".gnb").html();
     $(".mo_gnb_box .mo_gnb").html(gnbHtml);
